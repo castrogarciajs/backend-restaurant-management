ab686547a97b2ab4963eb6113f927d693d993709
Sebastian009w <ia3266595@gmail.com>
2023-06-12 00:40:57 -0500

    Install gin && init aplication with Gin #3

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	PORT := os.Getenv("PORT")
 
+	r := gin.New()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
 	if PORT == "" {
 		PORT = "8000"
 	}
+	r.Run()
 }
