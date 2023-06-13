3e1e9e3640649329cbf4f7e0aa84eac3ec4de1e9
Sebastian009w <ia3266595@gmail.com>
2023-06-12 22:04:07 -0500

    routes order item with gin #9

--- pkg/routes/order_item_router.go
+++ pkg/routes/order_item_router.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sebastian009w/backend-restaurant-management/app/controllers"
+)
+
+func OrderItemRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/ordersItems", controllers.GetOrdersItems())
+	incomingRoutes.GET("/ordersItems/:item_id", controllers.GetOrderItem())
+	incomingRoutes.GET("/orderItems-order/:order_id", controlers.GetOrderItemByOrder())
+	incomingRoutes.POST("/ordersItems", controllers.CreateOrderItem())
+	incomingRoutes.PATCH("ordersItems/:item_id", controllers.UpdateOrderItem())
+}
