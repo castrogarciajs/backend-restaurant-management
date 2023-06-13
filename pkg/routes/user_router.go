35ec6e03b12eecb4cd2a476b0cdb164d21e09f9e
Sebastian009w <ia3266595@gmail.com>
2023-06-12 21:34:18 -0500

    Routes User with Gin #9

--- pkg/routes/user_router.go
+++ pkg/routes/user_router.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sebastian009w/backend-restaurant-management/app/controllers"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/users", controllers.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+	incomingRoutes.POST("/users/singup", controllers.SingUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+}
