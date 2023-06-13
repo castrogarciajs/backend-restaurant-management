9ded2196fdc3bde38f094902cb77b3103bb2cccd
Sebastian009w <ia3266595@gmail.com>
2023-06-12 21:51:27 -0500

    routes menu with Gi #9

--- pkg/routes/menu_router.go
+++ pkg/routes/menu_router.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sebastian009w/backend-restaurant-management/app/controllers"
+)
+
+func MenuRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/menus", controllers.GetMenus())
+	incomingRoutes.GET("/menus/:food_id", controllers.GetMenu())
+	incomingRoutes.POST("/menus", controllers.CreateMenu())
+	incomingRoutes.PATCH("menus/:food_id", controllers.UpdateMenu())
+
+}
