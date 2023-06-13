33e8984b43231f5f56946f1422ab845ed3c35fec
Sebastian009w <ia3266595@gmail.com>
2023-06-12 21:41:24 -0500

    Routes food with Gin #9

--- pkg/routes/food_router.go
+++ pkg/routes/food_router.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sebastian009w/backend-restaurant-management/app/controllers"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods", controllers.GetFoods())
+	incomingRoutes.GET("/food/:food_id", controllers.GetFood())
+	incomingRoutes.POST("foods", controllers.CreateFood())
+	incomingRoutes.PATCH("food/:food_id", controllers.UpdateFood())
+
+}
