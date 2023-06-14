d7a3a931224302b21e7d9fb6d707d07a3ff3f291
Sebastian009w <ia3266595@gmail.com>
2023-06-13 21:12:44 -0500

    connection mongodb #14

--- platform/database/database_connection.go
+++ platform/database/database_connection.go
@@ -1,1 +1,34 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DbInstance() *mongo.Client {
+
+	MongoDB := "mongodb://127.0.0.1:27017"
+	fmt.Println(MongoDB)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
