aceeda01d39a7d1a9f2c039fa3e517e204e11fb6
Sebastian009w <ia3266595@gmail.com>
2023-06-13 21:24:56 -0500

    connection succesfuly with mongodb #14

--- platform/database/database_connection.go
+++ platform/database/database_connection.go
@@ -36,9 +36,9 @@ func DbInstance() *mongo.Client {
 
 var Client *mongo.Client = DbInstance()
 
-func OpenCollection(client *mongo.Client, collectionName string) mongo.Collection {
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 
 	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
 
-	return *collection
+	return collection
 }
