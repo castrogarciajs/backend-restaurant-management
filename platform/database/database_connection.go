7f9aeb7f569b8caeca5f1876f979e648a42c5f28
Sebastian009w <ia3266595@gmail.com>
2023-06-13 21:18:37 -0500

    name y collection to the database #14

--- platform/database/database_connection.go
+++ platform/database/database_connection.go
@@ -35,3 +35,10 @@ func DbInstance() *mongo.Client {
 }
 
 var Client *mongo.Client = DbInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) mongo.Collection {
+
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+
+	return *collection
+}
