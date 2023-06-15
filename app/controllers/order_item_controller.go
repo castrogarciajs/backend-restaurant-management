a8921677a3b372b35ea5a4bb9844743a0f068f5e
Sebastian009w <ia3266595@gmail.com>
2023-06-14 22:23:54 -0500

    order item controllers && table #15

--- app/controllers/order_item_controller.go
+++ app/controllers/order_item_controller.go
@@ -1,1 +1,27 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetOrdersItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
+
+func GetOrderItemByOrder() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
