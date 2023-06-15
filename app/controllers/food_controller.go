2e0437251d36653166243d0ee9a4ba559d2082a4
Sebastian009w <ia3266595@gmail.com>
2023-06-14 22:10:31 -0500

    controllers food  #15

--- app/controllers/food_controller.go
+++ app/controllers/food_controller.go
@@ -1,1 +1,25 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {}
+}
